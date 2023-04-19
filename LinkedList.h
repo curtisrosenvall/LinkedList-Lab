ce36f402cb2768823900e2bd2b662f8f790c2263
curtisrosenvall <70542777+curtisrosenvall@users.noreply.github.com>
2023-04-18 22:21:35 -0400

    done

--- LinkedList.h
+++ LinkedList.h
@@ -6,64 +6,173 @@
 
 template<typename T>
 class LinkedList: public LinkedListInterface<T> {
+private:
+    struct Node {
+        T data;
+        Node* next;
+
+        Node(T data, Node* next = nullptr) {
+            this->data = data;
+            this->next = next;
+        }
+    };
+
+    Node* head;
+    Node* tail;
+    size_t count;
 public:
-    LinkedList() {
-        // implement your constructor here
-    }
+    LinkedList() : head(nullptr), tail(nullptr), count(0) {}
 
     virtual ~LinkedList() {
-        // implement your destructor here
+        clear();
     }
 
     virtual void push_front(T item) {
-        // implement push_front here
+        head = new Node(item, head);
+        if (tail == nullptr) {
+            tail = head;
+        }
+        count++;
     }
 
     virtual void push_back(T item) {
-        // implement push_back here
+        Node* newNode = new Node(item);
+        if (tail != nullptr) {
+            tail->next = newNode;
+        } else {
+            head = newNode;
+        }
+        tail = newNode;
+        count++;
     }
 
     virtual void insert(T item, size_t position) {
-        // implement insert here
+        if (position > count) {
+            throw std::out_of_range("Position out of range");
+        }
+        if (position == 0) {
+            push_front(item);
+            return;
+        }
+        if (position == count) {
+            push_back(item);
+            return;
+        }
+        Node* currentNode = head;
+        for (size_t i = 0; i < position - 1; i++) {
+            currentNode = currentNode->next;
+        }
+        Node* newNode = new Node(item, currentNode->next);
+        currentNode->next = newNode;
+        count++;
     }
 
     virtual void pop_front() {
-        // implement pop_front here
+        if (head == nullptr) {
+            throw std::length_error("List is empty");
+        }
+        Node* oldHead = head;
+        head = head->next;
+        if (head == nullptr) {
+            tail = nullptr;
+        }
+        delete oldHead;
+        count--;
     }
 
     virtual void pop_back() {
-        // implement pop_back here
+        if (tail == nullptr) {
+            throw std::length_error("List is empty");
+        }
+        if (head == tail) {
+            pop_front();
+            return;
+        }
+        Node* currentNode = head;
+        while (currentNode->next != tail) {
+            currentNode = currentNode->next;
+        }
+        delete tail;
+        tail = currentNode;
+        tail->next = nullptr;
+        count--;
     }
 
     virtual void remove(size_t position) {
-        // implement remove here
+        if (position >= count) {
+            throw std::out_of_range("Position out of range");
+        }
+        if (position == 0) {
+            pop_front();
+            return;
+        }
+        if (position == count - 1) {
+            pop_back();
+            return;
+        }
+        Node* currentNode = head;
+        for (size_t i = 0; i < position - 1; i++) {
+            currentNode = currentNode->next;
+        }
+        Node* toDelete = currentNode->next;
+        currentNode->next = toDelete->next;
+        delete toDelete;
+        count--;
     }
 
     virtual T front() const {
-        // implement front here
+        if (head == nullptr) {
+            throw std::length_error("Cannot call front() on an empty list");
+        }
+        return head->data;
     }
 
     virtual T back() const {
-        // implement back here
+        if (tail == nullptr) {
+            throw std::length_error("Cannot call back() on an empty list");
+        }
+        return tail->data;
     }
 
     virtual T at(size_t index) const {
-        // implement at here
+        if (index >= count) {
+            throw std::out_of_range("Index out of range for list of size " + std::to_string(size()));
+        }
+        Node* currentNode = head;
+        for (size_t i = 0; i < index; i++) {
+            currentNode = currentNode->next;
+        }
+        return currentNode->data;
     }
 
     virtual bool contains(const T& item) const {
-        // implement contains here
+        Node* currentNode = head;
+        while (currentNode != nullptr) {
+            if (currentNode->data == item) {
+                return true;
+            }
+            currentNode = currentNode->next;
+        }
+        return false;
     }
 
     virtual size_t size() const {
-        // implement size here
+        return count;
     }
 
     virtual void clear() {
-        // implement clear here
+        while (head != nullptr) {
+            pop_front();
+        }
     }
 
     virtual std::string toString() const {
-        // implement toString here
+        std::string result;
+        Node* currentNode = head;
+        while (currentNode != nullptr) {
+            result += std::to_string(currentNode->data) + " ";
+            currentNode = currentNode->next;
+        }
+        return result;
     }
 };
---
+++ cmake-build-debug/CMakeFiles/scratch.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/scratch.dir/scratch.cpp.o"
+  "CMakeFiles/scratch.dir/scratch.cpp.o.d"
+  "scratch"
+  "scratch.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/scratch.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
